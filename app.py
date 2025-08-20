c2593b62fd2b9cd5797d66f97c4c8d2cf2b4d53b
Thomas Wotoro <didymusthomas6@gmail.com>
2025-08-20 21:13:31 +0300

    initial Commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_quotes():
+    """ This handles the GET requests from the API"""
+    quotes = ''
+    
+    # Fetching data from the API
+    url = 'https://zenquotes.io/api/random'
+    response = requests.get(url)
+    data = response.json()
+    
+    # Represent the data for use in the templates
+    quotes = {
+        "random_q": data[0]["q"],
+        "author": data[0]["a"]
+    }
+    
+    
+    return render_template('quotes.html', quotes=quotes)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+    
+    
